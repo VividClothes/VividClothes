30021dbe81603f60080ae77bbf1263e7d42017a4
dsyhwk1016 <kjsjsj1109@naver.com>
2022-05-25 00:13:50 +0900

    feature: Products, Categories 스키마 정의 및 모델 생성

---
+++ src/db/models/category-model.js
@@ -0,0 +1,45 @@
+import { model } from 'mongoose';
+import { CategorySchema } from '../schemas/category-schema';
+
+const Category = model('categories', CategorySchema);
+
+export class CategoryModel {
+    // 카테고리 추가
+    async create(categoryName) {
+        const createdNewCategory = await Category.create({ categoryName });
+        return createdNewCategory;
+    }
+
+    // 모든 document 출력
+    async findAll() {
+        const categories = await Category.find({});
+        return categories;
+    }
+
+    // 특정 카테고리만 출력
+    async findByCategory(categoryName) {
+        const category = await Category.findOne({ categoryName });
+        return category;
+    }
+    
+    // 카테고리 수정
+    async update({ categoryId, update }) {
+        const filter = { _id: categoryId };
+        const option = { returnOriginal: false };
+        
+        const updatedCategory = await Category.findOneAndUpdate(filter, update, option);
+        return updatedCategory;
+    }
+
+    // 카테고리 삭제
+    async delete(categoryId) {
+        const filter = { _id: categoryId };
+
+        const deleteCategory = await Category.findOneAndDelete(filter);
+        return deleteCategory;
+    }
+}
+
+const categoryModel = new CategoryModel();
+
+export { categoryModel };
