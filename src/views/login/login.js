edfbaabbb5981f0743f62bd021fc9ac95d44842a
siyah <sight41siyah@gmail.com>
2022-05-24 21:40:21 +0900

    mistake: 브랜치 수정

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -1,6 +1,5 @@
 import * as Api from '/api.js';
 import { validateEmail } from '/useful-functions.js';
-import { header } from '/header.js';
 
 // 요소(element), input 혹은 상수
 const emailInput = document.querySelector('#emailInput');
@@ -11,17 +10,11 @@ addAllElements();
 addAllEvents();
 
 // html에 요소를 추가하는 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
-async function addAllElements() {
-  insertHeader();
-}
+async function addAllElements() {}
 
 // 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
 function addAllEvents() {
   submitButton.addEventListener('click', handleSubmit);
-}
-
-function insertHeader() {
-  document.body.insertAdjacentElement('afterbegin', header);
 }
 
 // 로그인 진행
@@ -36,7 +29,9 @@ async function handleSubmit(e) {
   const isPasswordValid = password.length >= 4;
 
   if (!isEmailValid || !isPasswordValid) {
-    return alert('비밀번호가 4글자 이상인지, 이메일 형태가 맞는지 확인해 주세요.');
+    return alert(
+      '비밀번호가 4글자 이상인지, 이메일 형태가 맞는지 확인해 주세요.'
+    );
   }
 
   // 로그인 api 요청
@@ -48,7 +43,7 @@ async function handleSubmit(e) {
 
     // 로그인 성공, 토큰을 세션 스토리지에 저장
     // 물론 다른 스토리지여도 됨
-    localStorage.setItem('token', token);
+    sessionStorage.setItem('token', token);
 
     alert(`정상적으로 로그인되었습니다.`);
 
--- src/views/register/register.js
+++ src/views/register/register.js
@@ -13,17 +13,11 @@ addAllElements();
 addAllEvents();
 
 // html에 요소를 추가하는 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
-async function addAllElements() {
-  insertHeader();
-}
+async function addAllElements() {}
 
 // 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
 function addAllEvents() {
   submitButton.addEventListener('click', handleSubmit);
-}
-
-function insertHeader() {
-  document.body.insertAdjacentElement('afterbegin', header);
 }
 
 // 회원가입 진행
