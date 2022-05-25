b687ab99637e75ae9e779b75bbd5c7a55fa42adf
dnjsqhwo <dnjsqhwo@gmail.com>
2022-05-26 05:57:12 +0900

    feature: 헤더 디자인 변경, category ui 구현 및 api 요청 구현

--- src/views/shared/components/userHeaderList.js
+++ src/views/shared/components/userHeaderList.js
@@ -1,21 +1,25 @@
 export const userHeaderList = `
 <li>
-  <a class="navbar-item" href="/user-profile">회원정보</a>
+  <a class="color-whitesmoke link" href="/user-profile">회원정보</a>
+  <span class="color-whitesmoke mx-3">/</span>
 </li>
 <li>
-  <div class="navbar-item has-dropdown is-hoverable">
-    <a class="navbar-link  is-active">마이페이지</a>
-    <div class="navbar-dropdown ">
-      <a class="navbar-item " href="/user-order">
-        주문조회
+  <div class="dropdown">
+  <span class="dropdown-trigger color-whitesmoke link">마이 페이지<i class="fa-solid fa-angle-down ml-2"></i></span>
+  <div class="dropdown-menu" id="dropdown-menu3" role="menu">
+    <div class="dropdown-content">
+      <a href="/user-order" class="dropdown-item">
+        주문 조회
       </a>
-      <a class="navbar-item is-active" href="/cart">
+      <a href="/cart" class="dropdown-item ">
         장바구니
       </a>
     </div>
   </div>
+  <span class="color-whitesmoke mx-3">/</span>
+  </div>
 </li>
 
 <li>
-  <a class="logout" href="#logout" role="button">로그아웃</a>
+  <a class="logout color-whitesmoke link" href="#">로그아웃</a>
 </li>`;
