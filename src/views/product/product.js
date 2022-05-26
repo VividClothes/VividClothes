551f1e44485504309eb9bdb59ff47456553028e6
ChanLee <dlcks113@gmail.com>
2022-05-27 04:58:59 +0900

    feature: product 페이지 렌더링까지 구현 완료. 결제와 장바구니 추가 버튼 추가로 구현 필요

--- src/views/product/product.js
+++ src/views/product/product.js
@@ -1,4 +1,5 @@
 import * as Api from '/api.js';
+import { addCommas } from '/useful-functions.js';
 import { header } from '/header.js';
 
 /***************************헤더 내용**********************************/
@@ -20,56 +21,84 @@ function insertHeader() {
 /*********************************************************************/
 
 
-// 즉시실행 함수로 바로 실행
+
+/****************************요소 모음**********************************/
+const imageContainer = document.querySelector('.image-container');
+const productName = document.querySelector('.product-name');
+const productInfo = document.querySelector('.product-info');
+const priceText = document.querySelector('.price-text');
+const sizeText = document.querySelector('.size-text');
+const colorText = document.querySelector('.color-text');
+const totalPriceText = document.querySelector('.total-price-text');
+
+const quantity = document.querySelector('.quantity');
+const upButton = document.querySelector('.up-button');
+const downButton = document.querySelector('.down-button');
+/*********************************************************************/
+
+
+
+/****************************렌더링**********************************/
 (async () => {
     const urlParams = new URLSearchParams(window.location.search);
     const productID = urlParams.get('id');
     const result = await Api.get(`/product/${productID}`);
     console.log(result);
+
+    imageContainer.style.backgroundImage = `url( "${result.imagePath[0]}" )`;
+    productName.textContent = result.productName;
+    productInfo.textContent = result.info;
+    priceText.textContent = addCommas(result.price);
+    sizeText.textContent = result.option.size[0];
+    colorText.textContent = result.option.color[0];
+    totalPriceText.textContent = addCommas(result.price);
+
+
+
+    /********************수량 버튼 이벤트**************************/
+    // 수량 증가 버튼
+    upButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        
+        let quantityNum = parseInt(quantity.value);
+
+        if (quantityNum === 99) {
+            alert('1 이상 99 이하의 수량만 가능합니다.');
+        } else {
+            quantityNum++;
+            quantity.value = quantityNum;
+            totalPriceText.textContent = addCommas(quantityNum * result.price);
+        }
+    })
+
+    // 수량 감소 버튼
+    downButton.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        let quantityNum = parseInt(quantity.value);
+
+        if (quantityNum === 1) {
+            alert('1 이상 99 이하의 수량만 가능합니다.');
+        } else {
+            quantityNum--;
+            quantity.value = quantityNum;
+            totalPriceText.textContent = addCommas(quantityNum * result.price);
+        }
+    })
+    /*************************************************************/
+
+
+
+    /*********************수량 직접 입력***************************/
+    quantity.addEventListener('input', (e) => {
+        const quantityNum = parseInt(e.target.value);
+        if (quantityNum > 99 || quantityNum < 1) {
+            alert('1 이상 99 이하의 수량만 가능합니다.');
+            e.target.value = 1;
+            totalPriceText.textContent = addCommas(result.price);
+        } else { 
+            totalPriceText.textContent = addCommas(quantityNum * result.price);
+        }
+    })
+    /*************************************************************/
 })();
-
-
-
-/********************수량 버튼 이벤트*********************************/
-const quantity = document.querySelector('.quantity');
-const upButton = document.querySelector('.up-button');
-const downButton = document.querySelector('.down-button');
-
-// 수량 증가 버튼
-upButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    
-    let quantityNum = parseInt(quantity.value);
-
-    if (quantityNum === 99) {
-        alert('1 이상 99 이하의 수량만 가능합니다.');
-    } else {
-        quantityNum++;
-        quantity.value = quantityNum;
-    }
-})
-
-// 수량 감소 버튼
-downButton.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    let quantityNum = parseInt(quantity.value);
-
-    if (quantityNum === 1) {
-        alert('1 이상 99 이하의 수량만 가능합니다.');
-    } else {
-        quantityNum--;
-        quantity.value = quantityNum;
-    }
-})
-/*********************************************************************/
-
-
-// 임의로 입력 검사
-quantity.addEventListener('input', (e) => {
-    const quantityNum = parseInt(e.target.value);
-    if (quantityNum > 99 || quantityNum < 1) {
-        alert('1 이상 99 이하의 수량만 가능합니다.');
-        e.target.value = 1;
-    }
-})
