c508b5829afce406ba08df77f3760730b9b92c38
dsyhwk1016 <kjsjsj1109@naver.com>
2022-05-27 17:15:47 +0900

    refactor: 유저 유형 확인하는 미들웨어 추가 및 상태코드 수정

--- src/routers/order-router.js
+++ src/routers/order-router.js
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 import is from '@sindresorhus/is';
-import { loginRequired } from '../middlewares';
+import { loginRequired, userRoleCheck } from '../middlewares';
 import { orderService, userService, productService } from '../services';
 
 const orderRouter = Router();
 
 // 주문 등록
 // loginRequired 미들웨어 사용해 로그인한 유저만 주문 가능
-orderRouter.post('/register', loginRequired, async (req, res, next) => {
+orderRouter.post('/register',
+    loginRequired,
+    async (req, res, next) => {
     try {
         // Content-Type: application/json 설정을 안 한 경우, 에러를 만들도록 함.
         // application/json 설정을 프론트에서 안 하면, body가 비어 있게 됨.
@@ -53,22 +55,18 @@ orderRouter.post('/register', loginRequired, async (req, res, next) => {
         });
 
         // 추가된 주문 데이터를 프론트에 다시 보내줌
-        res.status(200).json(newOrder);
+        res.status(201).json(newOrder);
     } catch (error) {
         next(error);
     }
 });
 
 // 전체 주문 목록 조회 - 관리자 권한
-orderRouter.get('/list', loginRequired, async (req, res, next) => {
+orderRouter.get('/list',
+    loginRequired,
+    userRoleCheck,
+    async (req, res, next) => {
     try {
-        // 현재 로그인한 유저가 관리자가 아니라면 에러 발생
-        if (req.userRole != 'admin-user') {
-            throw new Error(
-                '관리자가 아닙니다. 다시 로그인해 주세요.'
-            );
-        }
-
         const orders = await orderService.getOrders();
 
         res.status(200).json(orders);
@@ -78,13 +76,15 @@ orderRouter.get('/list', loginRequired, async (req, res, next) => {
 })
 
 // 유저별 주문 목록 조회
-orderRouter.get('/list/:userId', loginRequired, async (req, res, next) => {
+orderRouter.get('/list/:userId',
+    loginRequired,
+    async (req, res, next) => {
     try {
         // req의 params에서 데이터 가져옴
         const { userId } = req.params;
 
         // basic-user가 다른 사람의 주문 목록을 확인할 수 없도록 제한
-        if (req.userRole != 'admin-user' && req.currentUserId != userId) {
+        if (req.currentUserRole != 'admin-user' && req.currentUserId != userId) {
             throw new Error(
                 '해당 유저의 주문 목록을 확인할 수 없습니다. 다시 로그인해 주세요.'
             );
@@ -99,15 +99,17 @@ orderRouter.get('/list/:userId', loginRequired, async (req, res, next) => {
 })
 
 // 특정 주문 내역 조회
-orderRouter.get('/:orderId', loginRequired, async (req, res, next) => {
+orderRouter.get('/:orderId',
+    loginRequired,
+    async (req, res, next) => {
     try {
         // req의 params에서 데이터 가져옴
         const { orderId } = req.params;
 
         // id를 기준으로 DB에서 주문 내역 조회
         const order = await orderService.getOrderById(orderId);
-        // 주문한 사람과 로그인한 사람이 일치하지 않으면 에러 발생
-        if (req.currentUserId != order.orderer) {
+        // basic-user가 다른 사람의 주문 목록을 확인할 수 없도록 제한
+        if (req.currentUserRole != 'admin-user' && req.currentUserId != order.orderer) {
             throw new Error(
                 '해당 주문 내역을 조회할 수 없습니다. 다시 로그인해 주세요.'
             )
@@ -120,17 +122,19 @@ orderRouter.get('/:orderId', loginRequired, async (req, res, next) => {
 })
 
 // 주문 정보 삭제 - 주문 취소
-orderRouter.delete('/cancel/:orderId', loginRequired, async (req, res, next) => {
+orderRouter.delete('/cancel/:orderId',
+    loginRequired,
+    async (req, res, next) => {
     try {
         // req의 params에서 데이터 가져옴
         const { orderId } = req.params;
 
         // id를 기준으로 DB에서 주문 내역 조회
         const order = await orderService.getOrderById(orderId);
-        // 주문한 사람과 로그인한 사람이 일치하지 않으면 에러 발생
-        if (req.currentUserId != order.orderer) {
+        // basic-user가 다른 사람의 주문을 취소할 수 없도록 제한
+        if (req.currentUserRole != 'admin-user' && req.currentUserId != order.orderer) {
             throw new Error(
-                '해당 주문 내역을 조회할 수 없습니다. 다시 로그인해 주세요.'
+                '해당 주문을 취소할 수 없습니다.'
             )
         }
 
