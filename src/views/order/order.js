439303a2c9cf03b2194861b88f895d14c7c51c0b
ChanLee <dlcks113@gmail.com>
2022-05-30 03:23:03 +0900

    feature: order 페이지 기능 구현 완료

--- src/views/order/order.js
+++ src/views/order/order.js
@@ -23,8 +23,120 @@ function insertHeader() {
 
 
 /***********************************주소 찾기******************************************/
-document.querySelector('.postcodeButton').addEventListener('click', postCodeButtonCallBack)
+const postCodeButton = document.querySelector('.postcodeButton');
+postCodeButton.addEventListener('click', postCodeButtonCallBack);
+/*************************************************************************************/
 
+
+
+/****************************요소 모음**********************************/
+const itemsBody = document.querySelector('.items-body');
+const orderButton = document.querySelector('.order-button');
+const recipient = document.querySelector('.recipient');
+const phone = document.querySelector('.phone');
+const postcode = document.querySelector('.postcode');
+const address1 = document.querySelector('.address1');
+const address2 = document.querySelector('.address2');
+const recipientArray = [recipient, phone, postcode, address1, address2];
+/*********************************************************************/
+
+
+
+// 상품 아이디 배열 - 결제하기 클릭하면 indexedDB에서 지워야함
+const productIdArray = [];
+
+
+
+(async () => {
+    
+    /*****************************쿼리스트링 값 추출*************************/
+    const urlParams = new URLSearchParams(window.location.search);
+    const storeName = urlParams.get('storeName');
+    /**********************************************************************/
+
+
+
+    // indexedDB 접속
+    const hashedEmail = localStorage.getItem('hashedEmail');
+    const onRequest = indexedDB.open(hashedEmail, 1);
+    onRequest.onsuccess = () => { 
+        const db = onRequest.result;
+        const transaction = db.transaction(storeName, 'readonly');
+        const results = transaction.objectStore(storeName).getAll();
+        
+        // indexedDB에서 데이터 가져오기 성공
+        results.onsuccess = () => {
+            let buyProducts = results.result;
+
+            if(storeName === 'cart') {
+                // 장바구니에서 넘어온 경우 체크된 상품들만 필터링
+                buyProducts = buyProducts.filter((product) => product.isChecked)
+            }
+
+
+            /*************************구매 상품 렌더링*****************************/ 
+            buyProducts.forEach((product) => {
+                itemsBody.insertAdjacentHTML('beforeend', makeItemHTML(product));
+                productIdArray.push(product.productId);
+            })
+            /*********************************************************************/
+
+
+
+            /*********************이미지 박스 이벤트 추가**************************/
+            const imageBoxes = itemsBody.getElementsByClassName('image-box');
+            Array.from(imageBoxes)
+                .forEach((imgBox, index) => {
+                    imgBox.style.backgroundImage = `url( "${buyProducts[index].imagePath}" )`;
+                    imgBox.addEventListener('click', (e) => {
+                        window.location.href = `/product/?id=${buyProducts[index].productId}`;
+                    })
+                });
+            /*********************************************************************/
+
+
+            
+            /*****************************주문 버튼 관련******************************/
+            orderButton.value = `${addCommas(getTotalPrice(buyProducts))}원 결제하기`;
+            orderButton.addEventListener('click', async (e) => {
+                e.preventDefault();
+                
+                // 주문 post 보낼때 상품 필드값
+                const productsInfo = getProductsInfo(buyProducts);
+
+                // 수령인 정보 객체로 가져오기. 빈칸 하나라도 존재하면 flase 반환.
+                const recipientInfo = getRecipientInfo(...recipientArray);
+                
+                if (recipientInfo) {
+                        
+                    const postData = {
+                        ...recipientInfo,
+                        products: productsInfo
+                    }
+                    
+                    // 서버에 주문 요청 
+                    await Api.post('/order/register', postData);
+                    
+                    // 주문 완료한 상품 indexedDB에서 삭제
+                    const transaction = db.transaction(storeName, 'readwrite');
+                    productIdArray.forEach(item => {
+                        transaction.objectStore(storeName).delete(item);
+                    })
+
+
+                    // 수정 필요
+                    alert('주문 완료! 주문 조회 페이지로 이동해야하는데 아직 구현 안해서 홈으로 이동');
+                    window.location.href = '/';
+                }
+            })
+            /*********************************************************************/
+        }
+    }
+})()
+
+
+
+// 주소 찾기 이벤트리스너 콜백함수
 function postCodeButtonCallBack(e) {
     e.preventDefault();
     new daum.Postcode({
@@ -51,4 +163,82 @@ function postCodeButtonCallBack(e) {
         }
     }).open();
 }
-/*************************************************************************************/
+
+
+// 구매 상품 HTML 생성 함수
+function makeItemHTML(product) {
+    return `
+    <div class="item-container">
+        <div class="product-name-options">
+            <div class="image-box"></div>
+            <div class="options-box">
+                <div class="product-name">${product.productName}</div>
+                <div class="options">옵션: ${product.size} / ${product.color}</div>
+            </div>
+        </div>
+        <div class="product-quantity">${product.quantity}</div>
+        <div class="total-price">${addCommas(product.price * product.quantity)}</div>
+    </div>
+    `
+}
+
+
+// 총 결제 금액 구해주는 함수
+function getTotalPrice(buyProducts) {
+    return buyProducts.reduce((totalPrice, product) => {
+        const addPrice = product.price * product.quantity;
+        return totalPrice + addPrice;
+    }, 0);
+}
+
+
+// 수령자 정보 확인
+function getRecipientInfo(...recipientArray) {
+    const recipient = recipientArray[0].value.trim();
+    const phoneNumber = recipientArray[1].value.trim();
+    const postalCode = recipientArray[2].value.trim();
+    const address1 = recipientArray[3].value.trim();
+    const address2 = recipientArray[4].value.trim()
+
+    if (!recipient) {
+        alert('수령인을 입력해주세요!')
+        return false;
+    }
+
+    if (!phoneNumber) {
+        alert('휴대전화를 입력해주세요!');
+        return false;
+    }
+
+    if (!postalCode) {
+        alert('우편번호 및 주소를 입력해주세요!');
+        return false;
+    }
+
+    if (!address2) {
+        alert('상세 주소를 입력해주세요!');
+        return false;
+    }
+
+    // 주문 완료하고 뒤로가기 후 0원 주문 방지
+    if (productIdArray.length === 0) {
+        alert('주문할 상품이 없습니다.');
+        return false;
+    }
+    return {
+        recipient,
+        postalCode,
+        address1,
+        address2,
+        phoneNumber
+    }
+}
+
+
+// 구매 상품들의 {productId, quantity}를 배열로 반환
+function getProductsInfo(buyProducts) {
+    return buyProducts.map((product) => ({
+        productId: product.productId,
+        quantity: product.quantity
+    }));
+}
