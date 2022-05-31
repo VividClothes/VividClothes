31512619abba5d81577331a5373ce0511c9c1c04
ChanLee <dlcks113@gmail.com>
2022-06-01 00:43:14 +0900

    feature: 주문 조회 페이지, 제품 상세 페이지 ui 구현 완료

---
+++ src/views/user-order-detail/user-order-detail.js
@@ -0,0 +1,172 @@
+import * as Api from '/api.js';
+import { addCommas, convertToNumber } from '/useful-functions.js';
+import { header } from '/header.js';
+
+/***************************헤더 내용**********************************/
+// 요소(element), input 혹은 상수
+addAllElements();
+addAllEvents();
+
+// html에 요소를 추가하는 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
+async function addAllElements() {
+  insertHeader();
+}
+
+// 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
+function addAllEvents() {}
+
+function insertHeader() {
+  document.body.insertAdjacentElement('afterbegin', header);
+}
+/*********************************************************************/
+
+
+
+/****************************요소 모음**********************************/
+const orderDate = document.querySelector('.order-date');
+const recipient = document.querySelector('.recipient');
+const phoneNumber = document.querySelector('.phone');
+const address = document.querySelector('.address');
+const itemsBody = document.querySelector('.items-body');
+const paymentPrice = document.querySelector('.payment-price');
+const PREPARE = '상품 준비중';
+const DELIVER = '상품 배송중';
+const COMPLETE = '배송 완료';
+/*********************************************************************/
+
+
+(async () => {
+    
+    /*****************************쿼리스트링 값 추출*************************/
+    const urlParams = new URLSearchParams(window.location.search);
+    const orderId = urlParams.get('id');
+    /**********************************************************************/
+
+    const ordersInfo = await Api.get(`/order/${orderId}`);
+    const orderItems = getProducts(ordersInfo);
+    const orderState = ordersInfo.state;
+
+
+    // 수령인 정보 설정
+    setRecipientInfo(ordersInfo);
+    
+
+    // 구매 상품 리스트 삽입
+    orderItems.forEach((item) => {
+        itemsBody.insertAdjacentHTML('beforeend', makeItemHTML(item, orderState));
+      })
+    
+
+    // 최종 결제 금액 표시
+    setPaymentPrice(orderItems);
+
+})()
+
+
+// 수령인 정보 설정 함수
+function setRecipientInfo(ordersInfo) {
+    const orderDateText = getDate(ordersInfo.createdAt);
+    const recipientText = ordersInfo.recipient;
+    const phoneNumberText = ordersInfo.phoneNumber;
+    const addressText = getAddress(ordersInfo.address);
+
+    orderDate.textContent = orderDateText;
+    recipient.textContent = recipientText;
+    phoneNumber.textContent = phoneNumberText;
+    address.textContent = addressText;
+}
+
+
+// 날짜 전처리
+function getDate(orderDate) {
+    let [ date, time ] = orderDate.split('T');
+    date = date.replaceAll('-', '.');
+    time = time.split('.')[0]
+    return `${date} ${time}`;
+}
+
+
+// 주소 전처리
+function getAddress(address) {
+    const { postalCode, address1, address2 } = address;
+    return `(${postalCode}) ${address1} ${address2}`;
+}
+
+
+// 응답 데이터로 구매 상품 데이터s 전처리
+function getProducts(ordersInfo) {
+    const items = [];
+
+    ordersInfo.products.forEach((order) => {
+        const item = {
+            productName: order.product[0].productName,
+            imagePath: order.product[0].imagePath[0],
+            size: order.option.size,
+            color: order.option.color,
+            quantity: order.quantity,
+            priceSum: order.quantity * order.product[0].price,
+            productId: order.product[0]._id
+        };
+
+        items.push(item);
+    })
+
+    return items;
+}
+
+
+// 구매 상품 HTML 생성 함수
+function makeItemHTML(item, orderState) {
+    return `
+    <div class="item-container">
+        <div class="product-name-options">
+            <div class="image-box"
+            style="background-image:url('${item.imagePath}')"
+            onclick="
+                window.location.href='/product?id=${item.productId}'
+            "></div>
+            <div class="options-box">
+                <div class="product-name">${item.productName}</div>
+                <div class="options">옵션: ${item.size} / ${item.color}</div>
+            </div>
+        </div>
+        <div class="product-quantity">${item.quantity}</div>
+        <div class="total-price">${addCommas(item.priceSum)}</div>
+        <div class="order-state-box">
+            ${stateComponent(orderState)}
+        </div>
+    </div>
+    `
+}
+
+
+// 상품 박스 내 '주문 상태' HTML
+function stateComponent(state) {
+    if (state === PREPARE) {
+      return `
+        <div class="order-state-text">${PREPARE}</div>
+        <a class="order-state-link">주문취소</a>
+      `
+    }
+  
+    else if (state === DELIVER) {
+      return `<div class="order-state-text">${DELIVER}</div>`
+    }
+  
+    else if(state === COMPLETE) {
+      return `
+        <div class="order-state-text">${COMPLETE}</div>
+        <a class="order-state-link">후기작성</a>
+      `
+    }
+  }
+
+
+
+  function setPaymentPrice(items) {
+      const totalPrice = items.reduce((sum, item) => {
+        return sum + item.priceSum;
+      }, 0);
+
+      paymentPrice.textContent = `${addCommas(totalPrice)}원`;
+  }
--- src/views/user-order-list/user-order-list.js
+++ src/views/user-order-list/user-order-list.js
@@ -24,56 +24,172 @@ function insertHeader() {
 
 /****************************요소 모음**********************************/
 const itemsBody = document.querySelector('.items-body');
+const productPrepareLink = document.querySelector('.product-prepare-link');
+const productDeliveryLink = document.querySelector('.product-delivery-link');
+const deliveryCompleteLink = document.querySelector('.delivery-complete-link');
+const PREPARE = '상품 준비중';
+const DELIVER = '상품 배송중';
+const COMPLETE = '배송 완료';
 /*********************************************************************/
 
 
 
 (async () => {
   // 데이터 불러오기
-  const orderedProducts = await Api.get(`/order/mylist`);
-  console.log(orderedProducts);
+  const orders = await Api.get(`/order/mylist`);
+  let orderItems = getOrderItems(orders);
+  const urlParams = new URLSearchParams(window.location.search);
+  const listType = urlParams.get('type');
+  console.log(orders)
+  console.log(orderItems);
+  
+  // 상품 준비중, 상품 배송중, 배송 완료 텍스트 및 개수 표시
+  setStateLinkText(orderItems);
+
+ 
+  /*****************쿼리스트링 있으면 나열 리스트 필터링*********************/
+  if (listType === 'prepare') {
+    orderItems = orderItems.filter((order) => order.state === PREPARE)
+  }
+  else if(listType === 'deliver') {
+    orderItems = orderItems.filter((order) => order.state === DELIVER)
+  }
+  else if(listType === 'complete') {
+    orderItems = orderItems.filter((order) => order.state === COMPLETE);
+  }
+  /***********************************************************************/
+
+
 
   /***********************주문내역 없을때 렌더링****************************/
-  if(orderedProducts.length === 0) {
+  if(orders.length === 0) {
     itemsBody.insertAdjacentHTML('beforeend', makeNoItemContainerHTML());
   } 
   /***********************************************************************/
 
 
+
   /********************주문내역 하나라도 있을때 렌더링***********************/
   else {
-    
+    orderItems.forEach((item) => {
+      itemsBody.insertAdjacentHTML('beforeend', makeItemContainerHTML(item));
+    })
   }
   /************************************************************************/
 })()
 
 
-function makeItemContainerHTML(product, orderId, orderDate) {
+
+// 개별 상품 HTML
+function makeItemContainerHTML(item) {
   return `
   <div class="item-container">
     <div class="product-info">
-      <div class="image-box"></div>
+      <div class="image-box" 
+      style="background-image:url('${item.imagePath}')"
+      onclick="
+        window.location.href='/product?id=${item.productId}'
+      "></div>
       <div class="options-box">
-        <div class="product-name">${product.productName}</div>
-        <div class="options">옵션: FREE / RED</div>
+        <div class="product-name">${item.productName}</div>
+        <div class="options">옵션: ${item.size} / ${item.color}</div>
       </div>
     </div>
-    <div class="order-date">2022-05-28</div>
+    <div class="order-date">${item.createdAt}</div>
     <div class="price-quantity">
-      <div class="price">24,000원</div>
-      <div class="quantity">1개</div>
+      <div class="price">${addCommas(item.priceSum)}원</div>
+      <div class="quantity">${item.quantity}개</div>
     </div>
     <div class="order-state-box">
-      <div class="order-state-text">상품 배송중</div>
+      ${stateComponent(item.state)}
     </div>
     <div class="order-detail">
-      <a href="#" class="order-detail-link">주문 상세보기</a>
+      <a href="/user-order-detail?id=${item.orderId}" class="order-detail-link">주문 상세보기</a>
     </div>
   </div>
   `
 }
 
 
+
+// 상품 존재하지 않을때
 function makeNoItemContainerHTML() {
-  return '<div class="no-item-container">주문 내역이 없습니다.</div>';
+  return '<div class="no-item-container">상품이 존재하지 않습니다.</div>';
+}
+
+
+
+// 상품 박스 내 '주문 상태' HTML
+function stateComponent(state) {
+  if (state === PREPARE) {
+    return `
+      <div class="order-state-text">${PREPARE}</div>
+      <a class="order-state-link">주문취소</a>
+    `
+  }
+
+  else if (state === DELIVER) {
+    return `<div class="order-state-text">${DELIVER}</div>`
+  }
+
+  else if(state === COMPLETE) {
+    return `
+      <div class="order-state-text">${COMPLETE}</div>
+      <a class="order-state-link">후기작성</a>
+    `
+  }
+}
+
+
+
+function setStateLinkText(orderItems) {
+  let [ prepare, deliver, complete ] = [ 0, 0, 0 ];
+  
+  orderItems.forEach((item) => {
+    if (item.state === PREPARE) prepare++;
+    if (item.state === DELIVER) deliver++;
+    if (item.state === COMPLETE) complete++;
+  })
+
+  productPrepareLink.textContent = `${PREPARE}(${prepare})`; 
+  productDeliveryLink.textContent = `${DELIVER}(${deliver})`;
+  deliveryCompleteLink.textContent = `${COMPLETE}(${complete})`;
+}
+
+
+
+// 응답 json 데이터 전처리
+function getOrderItems(orders) {
+  const orderItems = [];
+  
+  orders.forEach((order) => {
+    const sharedAttributes = {
+      orderId: order._id,
+      state: order.state,
+      createdAt: getDate(order.createdAt)
+    }
+
+    order.products.forEach((item) => {
+      const pushItem = {
+        ...sharedAttributes,
+        size: item.option.size,
+        color: item.option.color,
+        quantity: item.quantity,
+        priceSum: item.quantity * item.product[0].price,
+        imagePath: item.product[0].imagePath[0],
+        productName: item.product[0].productName,
+        productId: item.product[0]._id
+      }
+      orderItems.push(pushItem);
+    })
+  })
+
+  return orderItems;
+}
+
+
+
+// 날짜 전처리
+function getDate(date) {
+  return date.split('T')[0];
 }
