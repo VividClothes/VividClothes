dc8a8f1229f5f4886d8f23cc5bdc6bf25559d09b
ChanLee <dlcks113@gmail.com>
2022-06-01 00:42:12 +0900

    feature: 주문 상세 페이지 정적 경로 설정

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -28,6 +28,7 @@ viewsRouter.use('/product', serveStatic('product'));
 viewsRouter.use('/admin-category', serveStatic('admin-category'));
 viewsRouter.use('/admin-product', serveStatic('admin-product'));
 viewsRouter.use('/user-order-list', serveStatic('user-order-list'));
+viewsRouter.use('/user-order-detail', serveStatic('user-order-detail'));
 
 // ../views/images 내의 이미지 파일들을 쓸 수 있게 함
 viewsRouter.use(
