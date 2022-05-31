6d1c74348c5dc9efe48e3fc7bf132331f7ab9cad
siyah <sight41siyah@gmail.com>
2022-05-31 19:06:05 +0900

    style: 코드 포맷팅

--- src/routers/image-router.js
+++ src/routers/image-router.js
@@ -4,15 +4,19 @@ import { imageUpload } from '../middlewares';
 const imageRouter = Router();
 
 // 이미지 등록 api
-imageRouter.post('/register', imageUpload.array('images'), async (req, res, next) => {
+imageRouter.post(
+  '/register',
+  imageUpload.array('images'),
+  async (req, res, next) => {
     try {
-        // req.files에서 이미지 경로만 가져옴
-        const imagePath = req.files.map(image => image.location);
+      // req.files에서 이미지 경로만 가져옴
+      const imagePath = req.files.map((image) => image.location);
 
-        res.status(200).json(imagePath);
+      res.status(200).json(imagePath);
     } catch (error) {
-        next(error);
+      next(error);
     }
-});
+  }
+);
 
 export { imageRouter };
