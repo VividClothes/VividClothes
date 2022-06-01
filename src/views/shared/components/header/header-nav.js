64845fb93779fdad867e96d8cffd9d52f209d0ed
dnjsqhwo <dnjsqhwo@gmail.com>
2022-06-01 19:25:41 +0900

    refactor: 헤더 컴포넌트 코드 수정

--- src/views/shared/components/header/header-nav.js
+++ src/views/shared/components/header/header-nav.js
@@ -1,45 +1,94 @@
 import headerNavAdmin from './header-nav-admin.js';
 import headerNavUser from './header-nav-user.js';
 
-const headerNav = (props) => {
+const notLoginHeaderNav = () => /* html */ `
+<ul class="flex-style">
+  <li>
+    <a class="link" href="/login">로그인</a>
+    <span class="mx-3">/</span>
+  </li>
+  <li>
+    <a class="link" href="/register">회원가입</a>
+  </li>
+</ul>
+`;
+
+const loginHeaderNav = (config, dropdownContent) => /* html */ `
+  <ul class="flex-style">
+    <li>
+      <a class="link" href="/user-profile">회원정보</a>
+      <span class="mx-3">/</span>
+    </li>
+    
+    <li id="nav-mid">
+      <div class="dropdown">
+      <span class="dropdown-trigger link" role="button">
+        ${config}
+        <i class="fa-solid fa-angle-down ml-2"></i>
+      </span>
+      <div class="dropdown-menu" id="dropdown-menu3" role="menu">
+        <div class="dropdown-content">
+          ${dropdownContent()}
+        </div class="dropdown-content">
+      </div>
+      <span class="mx-3">/</span>
+      </div>
+    </li>
+  
+    <li>
+      <a class="logout link" href="#" role="button">로그아웃</a>
+    </li>
+  </ul>
+`;
+
+const createHeaderNav = (props) => {
   const isLogin = localStorage.getItem('token') ? true : false;
-  const isAdmin = localStorage.getItem('role') === 'admin-user' ? true : false;
+  const isAdmin = localStorage.getItem('role') === 'admin-user';
 
   const config = isAdmin ? '관리자 설정' : '마이 페이지';
   const dropdownContent = isAdmin ? headerNavAdmin : headerNavUser;
 
-  return /* html */ `
-    <ul class="flex-style">
-      <li>
-        <a class="link" href="/user-profile">회원정보</a>
-        <span class="mx-3">/</span>
-      </li>
-
-      ${
-        isLogin &&
-        /* html */ `
-          <li id="nav-mid">
-            <div class="dropdown">
-            <span class="dropdown-trigger link" role="button">
-              ${config}
-              <i class="fa-solid fa-angle-down ml-2"></i>
-            </span>
-            <div class="dropdown-menu" id="dropdown-menu3" role="menu">
-              <div class="dropdown-content">
-                ${dropdownContent()}
-              </div class="dropdown-content">
-            </div>
-            <span class="mx-3">/</span>
-            </div>
-          </li>
-          `
-      }
-
-      <li>
-        <a class="logout link" href="#" role="button">로그아웃</a>
-      </li>
-    </ul>
-  `;
+  return !isLogin ? notLoginHeaderNav() : loginHeaderNav(config, dropdownContent);
 };
 
-export default headerNav;
+/////////////////////////////////////////////////////////////////////////////////////
+const headerNav = document.createElement('div');
+headerNav.insertAdjacentHTML('afterbegin', createHeaderNav());
+
+function addHeaderNavEventListener() {
+  dropdownEventListener();
+  logoutEventListener();
+}
+
+function dropdownEventListener() {
+  const dropdownTrigger = headerNav.querySelector('.dropdown-trigger');
+  const dropdownMenu = headerNav.querySelector('.dropdown-menu');
+
+  if (!dropdownTrigger) return;
+
+  const dropdownHandler = () => {
+    dropdownTrigger.classList.toggle('clicked');
+    dropdownMenu.classList.toggle('show');
+  };
+
+  dropdownTrigger.addEventListener('click', dropdownHandler);
+}
+
+function logoutEventListener() {
+  const logoutBtn = headerNav.querySelector('.logout');
+
+  if (!logoutBtn) return;
+
+  const logoutHandler = (e) => {
+    e.preventDefault();
+    console.log('why?');
+    localStorage.removeItem('token');
+    localStorage.removeItem('role');
+    localStorage.removeItem('hashedEmail');
+    window.location.href = '/';
+  };
+
+  logoutBtn.addEventListener('click', (e) => logoutHandler(e));
+}
+
+export { headerNav, addHeaderNavEventListener };
--- src/views/shared/components/header/header.js
+++ src/views/shared/components/header/header.js
@@ -1,7 +1,7 @@
-import headerNav from '/header/header-nav.js';
+import { headerNav, addHeaderNavEventListener } from '/header/header-nav.js';
 import headerStyle from '/header/header-style.js';
 
-const header = () => {
+const createHeader = () => {
   return /* html */ `
   ${headerStyle}
   <header class="flex-style">
@@ -15,9 +15,29 @@ const header = () => {
       </a>
     </h1>
 
-    <div class="is-size-6 mr-6">${headerNav()}</div>
+    <div id="header-nav-root" class="is-size-6 mr-6"></div>
   </header>
   `;
 };
 
-export default header;
+const header = document.createElement('div');
+header.insertAdjacentHTML('afterbegin', createHeader());
+header.querySelector('#header-nav-root').insertAdjacentElement('afterbegin', headerNav);
+
+function addHeaderEventListener() {
+  hamburgerEventListnener();
+
+  addHeaderNavEventListener();
+}
+
+function hamburgerEventListnener() {
+  const hamburgerMenu = header.querySelector('.hamburger-menu');
+  const sidebar = document.querySelector('.sidebar');
+
+  const hamburgerHandler = () => {
+    sidebar.classList.add('open');
+  };
+  hamburgerMenu.addEventListener('click', hamburgerHandler);
+}
+
+export { header, addHeaderEventListener };
