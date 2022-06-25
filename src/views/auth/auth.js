7b81ac38cc9d38917d28ddd39a6a20822ae718fd
ChanLee <dlcks113@gmail.com>
2022-06-26 01:18:53 +0900

    fix: 로그아웃시 쿠키 삭제, auth 직접 입력시 자동 로그인 되는 버그 해결

--- src/views/auth/auth.js
+++ src/views/auth/auth.js
@@ -1,9 +1,4 @@
 import { loginQuery } from '/indexedDB.js';
-
-//쿠기 값 삭제
-const deleteCookie = (key) => {
-    document.cookie = key + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-  };
   
 //쿠기 값 가져오기
 const getCookieValue = (cookie) => {
@@ -20,14 +15,13 @@ const getCookieValue = (cookie) => {
 }
 
 const [token, userRole, hashedEmail] = getCookieValue(document.cookie);
+
 localStorage.setItem('token', token);
 localStorage.setItem('role', userRole);
 localStorage.setItem('hashedEmail', hashedEmail);
 
-const tokenCheck = localStorage.getItem('token');
-
 // indexedDB 생성
-if (tokenCheck) {
+if (token) {
     loginQuery(hashedEmail);
 
     alert(`정상적으로 로그인되었습니다.`);
