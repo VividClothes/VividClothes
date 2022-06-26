ed153c12edcda56ead0e64e78cf3347aacf345c8
dnjsqhwo <dnjsqhwo@gmail.com>
2022-06-26 09:08:35 +0900

    design: 타이틀 섹션 컴포넌트 반응형 추가

--- src/views/shared/components/layout/title-section-style.js
+++ src/views/shared/components/layout/title-section-style.js
@@ -3,23 +3,41 @@ const titleSectionStyle = (title) => {
 <style>
   .title-section-container {
     display: flex;
+    flex-direction: ${title === '관리자 설정' ? 'column' : 'row'};
     width: 100%;
     justify-content: space-between;
-    align-items: ${title !== '회원 정보' ? 'center' : 'flex-end'};
+    align-items: ${title === '회원 정보' ? 'flex-end' : 'center'};
   }
   
-  .title-section-title {
-    font-size: 2.5rem;
+  .title-section-container > .title-section-title {
+    font-size: 30px;
     font-weight: 600;
+    margin-bottom: ${title === '관리자 설정' ? '7.5px' : '0'};
   }
 
-  .title-section-left-side {
+  .title-section-title >.title-section-subtitle {
+    font-size: 20px;
+    font-style: italic;
+  }
+
+  .title-section-container > .title-section-left-side {
     display: flex;
     align-items: center;
   }
   
-  hr {
+  .title-section-hr {
     background-color: rgb(213, 213, 213);
+    margin: 15px 0;
+  }
+
+  @media screen and (min-width: 768px) {
+    .title-section-container {
+      flex-direction: row;
+    }
+    
+    .title-section-container > .title-section-title {
+      margin-bottom: 0;
+    }
   }
 
 </style>
