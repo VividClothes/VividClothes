24bdc23484c58331d0ad2ab7a93b99a1c36149f8
ChanLee <dlcks113@gmail.com>
2022-06-29 23:35:34 +0900

    feature: 홈 무한스크롤 적용

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,28 +1,44 @@
 import * as Api from '/api.js';
 import { header, addHeaderEventListener } from '/header/header.js';
 import { createCategory, addCategoryListener } from '/category/category.js';
+import {
+  handleResize,
+  getPerPage,
+  getBestItems,
+  getRecentItems,
+  makeMainImageHTML,
+  renderNewProducts
+} from '/home/handler-functions.js';
 
-/***************************헤더*************************************/
+
+/******************************필요 요소*****************************/
 const nav = document.getElementById('header');
 const navCategory = document.getElementById('category');
+const fetchMoreTrigger = document.querySelector("#fetchMore");
+const productGrid = document.querySelector('.grid');
+const slide = document.querySelector('.slide');
+const mainImageContainer = document.querySelector('.main-img-container');
+let [timer, page, perPage] = [null, 1, getPerPage(window.innerWidth)];
+/*******************************************************************/
+
+
 (async () => {
+
+  /***************************헤더*************************************/
   nav.insertAdjacentElement('afterbegin', header);
   const categories = await Api.get('/category/list');
   navCategory.insertAdjacentHTML('afterbegin', await createCategory({ categories }));
   addHeaderEventListener();
   addCategoryListener(navCategory);
-})();
-/*******************************************************************/
+  /*******************************************************************/
+  
 
-// 이미지 슬라이드 기능
-const slide = document.querySelector('.slide');
-const mainImageContainer = document.querySelector('.main-img-container');
-
-(async () => {
-  const result = await Api.get('/product/main');
-
-  const bestItems = getBestItems(result.popularProducts);
-  const recentItems = getRecentItems(result.recentProducts);
+  /**************************이미지 슬라이드*******************************/
+  const best_result = await Api.get('/product/main');
+  const recent_result = await Api.get('/product/main/recent', `?page=${page}&perPage=${perPage}`)
+  
+  const bestItems = getBestItems(best_result);
+  const recentItems = getRecentItems(recent_result.datas);
 
   const bestItemsImagePaths = bestItems.map((item) => item.imagePath);
   slide.style.backgroundImage = `url("${bestItemsImagePaths[0]}")`;
@@ -42,111 +58,25 @@ const mainImageContainer = document.querySelector('.main-img-container');
     setTimeout(showSlides, 3000);
   }
 
-  renderNewProducts(recentItems);
+  renderNewProducts(recentItems, productGrid);
+  /*******************************************************************/
+
+
+  /******************************무한 스크롤********************************/
+  window.addEventListener('resize', (e) => [timer, perPage] = handleResize(e, timer, perPage));
+
+  const fetchMoreObserver = new IntersectionObserver(([{ isIntersecting }]) => { if (isIntersecting) fetchMore() });
+  fetchMoreObserver.observe(fetchMoreTrigger);
+
+  const fetchMore = async () => {
+    fetchMoreTrigger.classList.add("loading");
+
+    const list = await Api.get('/product/main/recent', `?page=${++page}&perPage=${perPage}`)
+    const recentItems = getRecentItems(list.datas);
+    renderNewProducts(recentItems, productGrid)
+
+    fetchMoreTrigger.classList.remove("loading");
+  };
+
+  /*******************************************************************/
 })();
-
-function makeMainImageHTML(bestItems) {
-  return `
-  <div class="img1"
-  style='background-image: url("${bestItems[0].imagePath}")'
-  onclick='window.location.href="/product?id=${bestItems[0].productId}"'>
-    <div class="imgFloat"><div>BEST ${bestItems[0].category}</div></div>
-  </div>
-  <div class="img2"
-  style='background-image:url("${bestItems[1].imagePath}")'
-  onclick='window.location.href="/product?id=${bestItems[1].productId}"'>
-    <div class="imgFloat"><div>BEST ${bestItems[1].category}</div></div>
-  </div>
-  <div class="img3"
-  style='background-image:url("${bestItems[2].imagePath}")'
-  onclick='window.location.href="/product?id=${bestItems[2].productId}"'>
-    <div class="imgFloat"><div>BEST ${bestItems[2].category}</div></div>
-  </div>
-  <div class="img4"
-  style='background-image:url("${bestItems[3].imagePath}")'
-  onclick='window.location.href="/product?id=${bestItems[3].productId}"'>
-    <div class="imgFloat"><div>BEST ${bestItems[3].category}<div></div>
-  </div>
-  `;
-}
-
-function getBestItems(items) {
-  return items.map((item) => {
-    return {
-      category: item.product.category.categoryName,
-      imagePath: item.product.imagePath[0],
-      productId: item.product._id,
-      productName: item.product.productName,
-      price: item.product.price,
-      info: item.product.info,
-    };
-  });
-}
-
-function getRecentItems(items) {
-  return items.map((item) => {
-    return {
-      category: item.category.categoryName,
-      imagePath: item.imagePath[0],
-      productId: item._id,
-      productName: item.productName,
-      price: item.price,
-      info: item.info,
-    };
-  });
-}
-
-// 신상품 렌더링
-function renderNewProducts(recentItems) {
-  const categoryName = document.querySelector('.category-name');
-  const productGrid = document.querySelector('.main-content');
-
-  // grid
-  categoryName.innerHTML = `
-  <h2 class="title is-2">
-    <span class="is-italic is-capitalized is-size-4 ">New Arrivals</span>
-  </h2>
-  <hr>
-
-  `;
-
-  createProductsList(recentItems);
-
-  async function createProductsList(products) {
-    console.log(products);
-    const insertedEl = products
-      .map(({ productId, productName, price, imagePath, info }) => {
-        return `
-        <li class="col">
-          <a class="a-link" href=/product?id=${productId}&page=1>
-            <div class="card">
-              <div class="card-image">
-                <div class="img-container">
-                  <img class="grid-img" src=${imagePath} alt=${productName} />
-                  <div class="buynow">Buy Now</div>
-                </div>
-              </div>
-            </a>
-              
-              <div>
-                <div class="medias">
-                  <div class="media-content">
-                    <a class="a-link" href=/product?id=${productId}&page=1><p class="title font-16">${productName}</p></a>
-                  </div>
-                </div>
-
-                <div class="contents">
-                  <p class="font-15">${info}</p>
-                  <p class="subtitle"><i class="fa-solid fa-won-sign"></i> ${price.toLocaleString()}</p>
-                </div>
-              </div>
-            </div>
-        
-        </li>`;
-      })
-      .join('');
-    console.log(products);
-
-    productGrid.innerHTML = `<ul class="grid">${insertedEl}</ul>`;
-  }
-}
