e92525d580b075e7801ac78d255603ae7e331c15
ChanLee <dlcks113@gmail.com>
2022-07-01 00:42:12 +0900

    fix: 정규식 이전에 되던거 복원

--- src/views/shared/utils/useful-functions.js
+++ src/views/shared/utils/useful-functions.js
@@ -5,24 +5,22 @@ export const randomId = () => {
 
 // 이메일 형식인지 확인 (true 혹은 false 반환)
 export const validateEmail = (email) => {
-	return true;
-	// return String(email)
-	// 	.toLowerCase()
-	// 	.match(
-	// 		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-	// 	);
+	return String(email)
+		.toLowerCase()
+		.match(
+			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+		);
 };
 
 // 숫자에 쉼표를 추가함. (10000 -> 10,000)
 export const addCommas = (n) => {
-	return n;
-	// return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+	return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 };
 
 // 13,000원, 2개 등의 문자열에서 쉼표, 글자 등 제외 후 숫자만 뺴냄
 // 예시: 13,000원 -> 13000, 20,000개 -> 20000
 export const convertToNumber = (string) => {
-	return string;// return parseInt(string.replace(/(,|개|원)/g, ''));
+	return parseInt(string.replace(/(,|개|원)/g, ''));
 };
 
 // ms만큼 기다리게 함.
@@ -78,7 +76,7 @@ export const maskingFunc = {
 		// 	maskingStr = originStr.replace(/(?<=.{2})./gi, "*");
 		// }
 
-		// return maskingStr;
+		return maskingStr;
 	}
 }
 
