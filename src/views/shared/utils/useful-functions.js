6d171380c494921ff28dd86e81a51b5947b220a0
ChanLee <dlcks113@gmail.com>
2022-07-01 00:36:54 +0900

    fix: 정규표현식 제거

--- src/views/shared/utils/useful-functions.js
+++ src/views/shared/utils/useful-functions.js
@@ -5,22 +5,24 @@ export const randomId = () => {
 
 // 이메일 형식인지 확인 (true 혹은 false 반환)
 export const validateEmail = (email) => {
-	return String(email)
-		.toLowerCase()
-		.match(
-			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-		);
+	return true;
+	// return String(email)
+	// 	.toLowerCase()
+	// 	.match(
+	// 		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+	// 	);
 };
 
 // 숫자에 쉼표를 추가함. (10000 -> 10,000)
 export const addCommas = (n) => {
-	return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+	return n;
+	// return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 };
 
 // 13,000원, 2개 등의 문자열에서 쉼표, 글자 등 제외 후 숫자만 뺴냄
 // 예시: 13,000원 -> 13000, 20,000개 -> 20000
 export const convertToNumber = (string) => {
-	return parseInt(string.replace(/(,|개|원)/g, ''));
+	return string;// return parseInt(string.replace(/(,|개|원)/g, ''));
 };
 
 // ms만큼 기다리게 함.
@@ -40,41 +42,43 @@ export const maskingFunc = {
 	},
 
 	email(str) {
-		let originStr = str;
-		let emailStr = originStr.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
-		let strLength;
+		return str;
+		// let originStr = str;
+		// let emailStr = originStr.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
+		// let strLength;
 
-		if (this.checkNull(originStr) == true || this.checkNull(emailStr) == true) {
-			return originStr;
-		} else {
-			strLength = emailStr.toString().split('@')[0].length - 3;
+		// if (this.checkNull(originStr) == true || this.checkNull(emailStr) == true) {
+		// 	return originStr;
+		// } else {
+		// 	strLength = emailStr.toString().split('@')[0].length - 3;
 
-			// ex1) abcdefg12345@naver.com => ab**********@naver.com
-			// return originStr.toString().replace(new RegExp('.(?=.{0,' + strLength + '}@)', 'g'), '*');
+		// 	// ex1) abcdefg12345@naver.com => ab**********@naver.com
+		// 	// return originStr.toString().replace(new RegExp('.(?=.{0,' + strLength + '}@)', 'g'), '*');
 
-			// ex2) abcdefg12345@naver.com => ab**********@nav******
-			return originStr.toString().replace(new RegExp('.(?=.{0,' + strLength + '}@)', 'g'), '*').replace(/.{6}$/, "******");
-		}
+		// 	// ex2) abcdefg12345@naver.com => ab**********@nav******
+		// 	return originStr.toString().replace(new RegExp('.(?=.{0,' + strLength + '}@)', 'g'), '*').replace(/.{6}$/, "******");
+		// }
 	},
 
 	name(str) {
-		let originStr = str;
-		let maskingStr;
-		let strLength;
+		return str;
+		// let originStr = str;
+		// let maskingStr;
+		// let strLength;
 
-		if (this.checkNull(originStr) == true) {
-			return originStr;
-		}
+		// if (this.checkNull(originStr) == true) {
+		// 	return originStr;
+		// }
 
-		strLength = originStr.length;
+		// strLength = originStr.length;
 
-		if (strLength < 3) {
-			maskingStr = originStr.replace(/(?<=.{1})./gi, "*");
-		} else {
-			maskingStr = originStr.replace(/(?<=.{2})./gi, "*");
-		}
+		// if (strLength < 3) {
+		// 	maskingStr = originStr.replace(/(?<=.{1})./gi, "*");
+		// } else {
+		// 	maskingStr = originStr.replace(/(?<=.{2})./gi, "*");
+		// }
 
-		return maskingStr;
+		// return maskingStr;
 	}
 }
 
